529be26725052d0f0f4afba5aaf4143403747607
Louis Normand <louis.normand@live.fr>
2023-09-23 22:02:59 +0200

    generic spider la central

---
+++ app/spiders/la_centrale/la_centrale_bmw_z3_spider.py
@@ -0,0 +1,15 @@
+from typing import Iterable
+from scrapy import Selector
+from scrapy.http import Request
+from app.spiders.la_centrale.la_centrale_spider import LaCentraleSpider
+
+
+class LaCentraleBmwZ3Spider(LaCentraleSpider):
+    name = "la_centrale_bmw_z3"
+
+    def start_requests(self) -> Iterable[Request]:
+        return super().start_requests(self.parse, "BMW", "Z3", "2000")
+
+    def parse(self, response: Selector):
+        stats = super().get_stats(response)
+        return stats
---
+++ app/spiders/la_centrale/la_centrale_spider.py
@@ -0,0 +1,56 @@
+from functools import reduce
+from typing import Iterable, List
+from scrapy import Selector, Spider
+from scrapy.http import Request
+
+from app.helpers.dom_parser import parse_style
+
+
+class LaCentraleSpider(Spider):
+    threadshold_2k = 15
+
+    custom_settings = {
+        "FEEDS": {"data/%(name)s/%(name)s_%(time)s.json": {"format": "json"}}
+    }
+
+    def start_requests(
+        self, parse, brand: str, model: str, maxYear: str
+    ) -> Iterable[Request]:
+        url = "https://www.lacentrale.fr/listing?categories=46%2C45&makesModelsCommercialNames={0}%3A{1}&yearMax={2}".format(
+            brand, model, maxYear
+        )
+        yield Request(url=url, callback=parse)
+
+    def get_stats(self, response: Selector):
+        chartBars = response.xpath(
+            "//div[@class='minMaxChart__chartBars active']/*"
+        ).getall()
+        data = []
+        for bar in chartBars:
+            data.append(parse_style(html=bar, elements=["height"]))
+        data_as_list = sum(data, [])
+        percentage_max_size = reduce(lambda x, y: float(x) + float(y), data_as_list, 0)
+        price_percentage_list = list(
+            map(lambda x: (float(x) * 100) / percentage_max_size, data_as_list)
+        )
+        _average_price = 0
+        _median = 0
+        median_index = 0
+        for index, price_percentage in enumerate(price_percentage_list):
+            _average_price += self.get_price_per_index(index) * price_percentage
+
+            if _median < 50:
+                _median += price_percentage
+                median_index = index
+        _average_price /= 100
+        _median = self.get_price_per_index(median_index)
+        stats = {}
+        stats["average_price"] = round(_average_price, 2)
+        stats["median"] = _median
+        return stats
+
+    def get_price_per_index(self, index: int):
+        if index > self.threadshold_2k:
+            return 2000 * self.threadshold_2k + 5000 * (index - self.threadshold_2k)
+        else:
+            return 2000 * index
