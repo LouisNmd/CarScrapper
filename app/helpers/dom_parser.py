5c87811dd1e8d6c9646f04efff5cccadad88098e
Louis Normand <louis.normand@live.fr>
2023-09-24 12:27:05 +0200

    docstring dom_parser

--- app/helpers/dom_parser.py
+++ app/helpers/dom_parser.py
@@ -1,12 +1,20 @@
 from typing import List
 
 
-def parse_style(html: str, elements: List[str]) -> str:
+def parse_style(html: str, elements: List[str]) -> List[dict]:
+    """
+    Parse l'attribut 'style' d'une balise HTML, et retourne les valeurs des sous-attributs recherchés
+
+    Args:
+        html (str): la balise HTML à parser (exemple: <div style="..." id="..." class="..."></div>)
+        elements (List[str]): Liste des clés à rechercher dans la balise 'style' (exemple: height, width, background-color, etc...)
+
+    Returns:
+        List[dict]: Liste de dictionnaire {'élément_à_rechercher': 'valeur'}
+    """
     dom = html.partition(" ")[0].removeprefix("<")
     styles_as_str = ""
-    prefix = "<" + dom
-    suffix = "></" + dom + ">"
-    attributes = html.removeprefix(prefix).removesuffix(suffix)
+    attributes = html.removeprefix("<" + dom).removesuffix("></" + dom + ">")
     for att in attributes.split(" "):
         if (att).startswith('style="'):
             styles_as_str = att.removeprefix("style=").replace('"', "")
@@ -16,6 +24,6 @@ def parse_style(html: str, elements: List[str]) -> str:
     for style in styles_as_list:
         style_attribute = style.split(":")
         if style_attribute[0] in elements:
-            result.append(style_attribute[1].replace("px", ""))
+            result.append({style_attribute[0]: style_attribute[1].replace("px", "")})
 
     return result
