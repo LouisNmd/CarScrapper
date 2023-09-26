6396e0168b285fdfa563038ea38147675cbeb891
Louis Normand <louis.normand@live.fr>
2023-09-26 21:25:13 +0200

    scrapy scheduler

--- back/scraper/app/main.py
+++ back/scraper/app/main.py
@@ -4,6 +4,7 @@ import os
 
 print("Scheduler initialised")
 schedule.every(30).seconds.do(lambda: os.system("scrapy crawl la_centrale_mazda_mx5"))
+schedule.every(30).seconds.do(lambda: os.system("scrapy crawl la_centrale_bmw_z3"))
 print("Next job is set to run at: " + str(schedule.next_run()))
 
 while True:
